d9042b34d43d39b468e2ad99fbfd970b344b336d
Gustavo Dias <gustavodias0904@gmail.com>
2024-11-05 17:12:52 -0300

    Adição de novo código

---
+++ Aeds 1/Códigos a parte - Teste de aprendizado/struct pt1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+//Criação da struct
+typedef struct pizza {
+    char nome[50];
+    float preco;
+    char tamanho [15];
+};
+
+//Declaração para uso sem struct
+typedef struct pizza pizza;
+
+int main (void){
+    setlocale (LC_ALL, "Portuguese");
+    pizza p;
+
+    //Recebe o nome, preço e tamanho
+    puts ("Digite o nome da pizza: ");
+    fgets (p.nome, 50, stdin);
+    fflush (stdin);
+    puts ("\nDigite o preço da pizza: ");
+    scanf ("%f", &p.preco);
+    fflush (stdin);
+    puts ("\nDigite o tamanho da pizza: ");
+    fgets (p.tamanho, 50, stdin);
+    fflush (stdin);
+
+    printf ("\n\n--------------- Pizza informada ---------------\n");
+    printf ("\tNome: %s", p.nome);
+    printf ("\tPreço: %.2f\n", p.preco);
+    printf ("\tTamanho: %s", p.tamanho);
+}
