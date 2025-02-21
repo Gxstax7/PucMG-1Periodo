046658cad3fa79b441f3b22aafa0d63adcf425a9
Gustavo Henrique dos Santos Dias <gustavodias0904@gmail.com>
2025-02-21 00:58:35 -0300

    Add files via upload

---
+++ TI2/Ex01/Ex01.java
@@ -0,0 +1,15 @@
+package Ex01;
+import java.util.*;
+
+public class Ex01 {
+	
+	public static void main (String [] args) {
+		Scanner sc = new Scanner (System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.println("A soma é: " + (num1 + num2));
+		sc.close();
+	}
+
+}
