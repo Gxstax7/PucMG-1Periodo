cac0b8bf086a7e379df0cdad6e0bfcb67912cab5
Gustavo Dias <gustavodias0904@gmail.com>
2025-03-02 21:10:24 -0300

    TP 01 aeds

---
+++ Aeds 2/TP 01/Q12/Q12.java
@@ -0,0 +1,36 @@
+/*
+ Linha de pensamento: 4 validadores, se um for falso a senha é invalida e retorna false; 4 condicionais para validar e caso falsas alteram a resposta para falsa e retorna a resposta.
+*/
+
+import java.util.*;
+
+class Q12{
+
+	public boolean validaSenha (String senha){
+		
+		boolean digito = false;
+		boolean letraMinuscula = false;
+		boolean letraMaiuscula = false;
+		boolean caractere = false;
+
+		for (char c : senha.){
+		
+			if (){
+			
+			}
+
+		}
+		return true;
+	}
+
+	public static void main (String args []){
+	
+		String senha = " ";
+		senha = MyIO.readLine();
+		while (!(senha.equals = ("FIM"))){
+			System.out.println (validaSenha(senha) ? "SIM" : "NAO");
+		}
+
+	}
+
+}
