7b8332688c13e8b06409c0620000814f1c2f5a55
Gustavo Dias <gustavodias0904@gmail.com>
2025-04-10 17:38:33 -0300

    TP-2 atualizado

--- Aeds 2/TP02/SHOW.java
+++ Aeds 2/TP02/SHOW.java
@@ -2,254 +2,271 @@ import java.util.*;
 import java.io.*;
 
 class SHOW {
-	
-	private String SHOW_ID, TITLE, TYPE, DIRECTOR, COUNTRY, RATING, DURATION, DATE_ADDED, DESCRIPTION;
-	private ArrayList<String> CAST = new ArrayList<>();
-	private ArrayList<String> LISTED_IN = new ArrayList<>();
-	private int RELEASE_YEAR;
 
-	// ------------------------
-	// Getters e Setters
-	// ------------------------
-	
-	public String getID() {
-		return SHOW_ID;
-	}
+    private String SHOW_ID, TITLE, TYPE, DIRECTOR, COUNTRY, RATING, DURATION, DATE_ADDED;
+    private ArrayList<String> CAST = new ArrayList<>();
+    private ArrayList<String> LISTED_IN = new ArrayList<>();
+    private int RELEASE_YEAR;
 
-	public void setID(String SHOW_ID) {
-		this.SHOW_ID = SHOW_ID;
-	}
+    public SHOW() {
+        setID("");
+        setTITLE("");
+        setTYPE("");
+        setDIRECTOR("");
+        setCOUNTRY("");
+        setRATING("");
+        setDURATION("");
+        setDATE("");
+        setRELEASE_YEAR(-1);
+        setCAST(new ArrayList<String>());
+        setLISTED_IN(new ArrayList<String>());
+    }
 
-	public String getDESCRIPTION() {
-		return DESCRIPTION;
-	}
+    public SHOW(String SHOW_ID, String TITLE, String TYPE, String DIRECTOR, ArrayList<String> CAST,
+                String COUNTRY, String DATE_ADDED, int RELEASE_YEAR, String RATING,
+                String DURATION, ArrayList<String> LISTED_IN) {
+        setID(SHOW_ID);
+        setTITLE(TITLE);
+        setTYPE(TYPE);
+        setDIRECTOR(DIRECTOR);
+        setCAST(CAST);
+        setCOUNTRY(COUNTRY);
+        setDATE(DATE_ADDED);
+        setRELEASE_YEAR(RELEASE_YEAR);
+        setRATING(RATING);
+        setDURATION(DURATION);
+        setLISTED_IN(LISTED_IN);
+    }
 
-	public void setDESCRIPTION(String DESCRIPTION) {
-		this.DESCRIPTION = DESCRIPTION;
-	}
+    public String getID() {
+        return SHOW_ID;
+    }
+    public void setID(String SHOW_ID) {
+        this.SHOW_ID = SHOW_ID;
+    }
 
-	public String getDATE() {
-		return DATE_ADDED;
-	}
+    public String getDATE() {
+        return DATE_ADDED;
+    }
+    public void setDATE(String DATE_ADDED) {
+        this.DATE_ADDED = DATE_ADDED;
+    }
 
-	public void setDATE(String DATE_ADDED) {
-		this.DATE_ADDED = DATE_ADDED;
-	}
+    public String getTITLE() {
+        return TITLE;
+    }
+    public void setTITLE(String TITLE) {
+        this.TITLE = TITLE;
+    }
 
-	public String getTITLE() {
-		return TITLE;
-	}
+    public String getTYPE() {
+        return TYPE;
+    }
+    public void setTYPE(String TYPE) {
+        this.TYPE = TYPE;
+    }
 
-	public void setTITLE(String TITLE) {
-		this.TITLE = TITLE;
-	}
+    public String getDIRECTOR() {
+        return DIRECTOR;
+    }
+    public void setDIRECTOR(String DIRECTOR) {
+        this.DIRECTOR = DIRECTOR;
+    }
 
-	public String getTYPE() {
-		return TYPE;
-	}
+    public String getCOUNTRY() {
+        return COUNTRY;
+    }
+    public void setCOUNTRY(String COUNTRY) {
+        this.COUNTRY = COUNTRY;
+    }
 
-	public void setTYPE(String TYPE) {
-		this.TYPE = TYPE;
-	}
+    public String getRATING() {
+        return RATING;
+    }
+    public void setRATING(String RATING) {
+        this.RATING = RATING;
+    }
 
-	public String getDIRECTOR() {
-		return DIRECTOR;
-	}
+    public String getDURATION() {
+        return DURATION;
+    }
+    public void setDURATION(String DURATION) {
+        this.DURATION = DURATION;
+    }
 
-	public void setDIRECTOR(String DIRECTOR) {
-		this.DIRECTOR = DIRECTOR;
-	}
+    public ArrayList<String> getCAST() {
+        return CAST;
+    }
+    public void setCAST(ArrayList<String> CAST) {
+        this.CAST = CAST;
+    }
 
-	public String getCOUNTRY() {
-		return COUNTRY;
-	}
+    public ArrayList<String> getLISTED_IN() {
+        return LISTED_IN;
+    }
+    public void setLISTED_IN(ArrayList<String> LISTED_IN) {
+        this.LISTED_IN = LISTED_IN;
+    }
 
-	public void setCOUNTRY(String COUNTRY) {
-		this.COUNTRY = COUNTRY;
-	}
+    public int getRELEASE_YEAR() {
+        return RELEASE_YEAR;
+    }
+    public void setRELEASE_YEAR(int RELEASE_YEAR) {
+        this.RELEASE_YEAR = RELEASE_YEAR;
+    }
 
-	public String getRATING() {
-		return RATING;
-	}
+    public SHOW clone() {
+        SHOW clone = new SHOW();
+        clone.setID(getID());
+        clone.setTITLE(getTITLE());
+        clone.setTYPE(getTYPE());
+        clone.setDIRECTOR(getDIRECTOR());
+        clone.setCAST(new ArrayList<>(getCAST()));
+        clone.setCOUNTRY(getCOUNTRY());
+        clone.setDATE(getDATE());
+        clone.setRELEASE_YEAR(getRELEASE_YEAR());
+        clone.setRATING(getRATING());
+        clone.setDURATION(getDURATION());
+        clone.setLISTED_IN(new ArrayList<>(getLISTED_IN()));
+        return clone;
+    }
 
-	public void setRATING(String RATING) {
-		this.RATING = RATING;
-	}
+    public static void imprimir(ArrayList<SHOW> show, String IdDesejado) {
+        SHOW escolhido = new SHOW();
+        int i = 0;
 
-	public String getDURATION() {
-		return DURATION;
-	}
+        while (i < show.size()) {
+            if (IdDesejado.equals(show.get(i).getID())) {
+                escolhido = show.get(i).clone();
+                break;
+            }
+            i++;
+        }
 
-	public void setDURATION(String DURATION) {
-		this.DURATION = DURATION;
-	}
+        if (i == show.size()) {
+            System.out.println("Id nao encontrado");
+        } else {
+            System.out.println("=> " + escolhido.getID() + " ## " + escolhido.getTITLE() + " ## " + escolhido.getTYPE()
+                    + " ## " + escolhido.getDIRECTOR() + " ## " + escolhido.getCAST() + " ## " + escolhido.getCOUNTRY()
+                    + " ## " + escolhido.getDATE() + " ## " + escolhido.getRELEASE_YEAR() + " ## " + escolhido.getRATING()
+                    + " ## " + escolhido.getDURATION() + " ## " + escolhido.getLISTED_IN() + " ##");
+        }
+    }
 
-	public ArrayList<String> getCAST() {
-		return CAST;
-	}
+    public static ArrayList<String> splitArrays(String frase) {
+        ArrayList<String> separado = new ArrayList<>();
+        int i = 0;
 
-	public void setCAST(ArrayList<String> CAST) {
-		this.CAST = CAST;
-	}
+        while (i < frase.length()) {
+            if (frase.equals("NaN")) {
+                separado.add("NaN");
+                break;
+            } else {
+                StringBuilder aux = new StringBuilder();
+                while (i < frase.length() && frase.charAt(i) != ',') {
+                    aux.append(frase.charAt(i));
+                    i++;
+                }
+                separado.add(aux.toString());
+            }
+            i += 2;
+        }
 
-	public ArrayList<String> getLISTED_IN() {
-		return LISTED_IN;
-	}
+        return separado;
+    }
 
-	public void setLISTED_IN(ArrayList<String> LISTED_IN) {
-		this.LISTED_IN = LISTED_IN;
-	}
+    public static SHOW atribuiValores(String linha) {
+        int i = 0;
+        SHOW show = new SHOW();
+        ArrayList<String> separa = new ArrayList<>();
 
-	public int getRELEASE_YEAR() {
-		return RELEASE_YEAR;
-	}
+        while (i < linha.length()) {
+            if (linha.charAt(i) == '"') {
+                i++;
+                StringBuilder aux = new StringBuilder();
+                String prox = "";
 
-	public void setRELEASE_YEAR(int RELEASE_YEAR) {
-		this.RELEASE_YEAR = RELEASE_YEAR;
-	}
+                while (i + 1 < linha.length()) {
+                    prox += linha.charAt(i);
+                    prox += linha.charAt(i + 1);
 
-	
-	public static ArrayList<String> splitArrays (String frase){
+                    if (linha.charAt(i) == '"') {
+                        if (linha.charAt(i + 1) == ',') {
+                            i++;
+                            break;
+                        }
+                        i++;
+                    }
+                    aux.append(linha.charAt(i));
+                    i++;
+                    prox = "";
+                }
 
-		ArrayList<String> separado = new ArrayList<>();
-		int i = 0;
+                separa.add(aux.toString());
+            } else {
+                if (i < linha.length() && linha.charAt(i) == ',') {
+                    if (linha.charAt(i + 1) == ',') {
+                        separa.add("NaN");
+                    }
+                    i++; // Pula a vírgula
+                } else {
+                    StringBuilder aux = new StringBuilder();
+                    while (i < linha.length() && linha.charAt(i) != ',') {
+                        aux.append(linha.charAt(i));
+                        i++;
+                    }
+                    separa.add(aux.toString());
+                }
+            }
+        }
 
-		while(i < frase.length()){
+        show.setID(separa.get(0));
+        show.setTYPE(separa.get(1));
+        show.setTITLE(separa.get(2));
+        show.setDIRECTOR(separa.get(3));
+        show.setCAST(splitArrays(separa.get(4)));
+        show.setCOUNTRY(separa.get(5));
+        show.setDATE(separa.get(6));
+        show.setRELEASE_YEAR(Integer.parseInt(separa.get(7)));
+        show.setRATING(separa.get(8));
+        show.setDURATION(separa.get(9));
+        show.setLISTED_IN(splitArrays(separa.get(10)));
 
-			if(frase.equals("NaN")){
-				separado.add("NaN");
-				break;
-			}else{
-				StringBuilder aux = new StringBuilder();
-				while(i < frase.length() && frase.charAt(i) != ','){
-					aux.append(frase.charAt(i));
-					i++;
-				}
-				separado.add(aux.toString());
-			}
-			i+=2;
-		}
- 
-		return separado;
-	}
+        return show;
+    }
 
+    public static ArrayList<SHOW> ler() {
+        String linha = "";
+        int aux = 0;
+        ArrayList<SHOW> show = new ArrayList<>();
 
-	public static SHOW atribuiValores (String linha){
-		
-		int i = 0;
-		SHOW show = new SHOW();
-		ArrayList<String> separa = new ArrayList<>();
-		
-		//--------------------------------------------------------
-		//Lógica para realizar o split de acordo com os atributos
-		//--------------------------------------------------------
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("/tmp/disneyplus.csv"));
+            while ((linha = br.readLine()) != null) {
+                if (aux >= 1) {
+                    show.add(atribuiValores(linha));
+                }
+                aux++;
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println("Erro!!");
+        }
 
-		while (i < linha.length()){
+        return show;
+    }
 
-			if(linha.charAt(i) == '"'){
-				i++;
-				StringBuilder aux = new StringBuilder();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<SHOW> show = ler();
+        String idDesejado = sc.nextLine();
 
-				String prox = "";
+        while (!idDesejado.equals("FIM")) {
+            imprimir(show, idDesejado);
+            idDesejado = sc.nextLine();
+        }
 
-				while(i+1 < linha.length()){
-					prox += linha.charAt(i);
-					prox += linha.charAt(i+1);
-
-					if(linha.charAt(i) == '"'){
-						if (linha.charAt(i+1) == ','){
-							i++;
-							break;
-						}
-						i++;
-					}
-						aux.append(linha.charAt(i));
-						i++;
-						prox = "";
-				}
-				separa.add(aux.toString());
-	
-				//Pula a segunda aspas
-			}else{
-
-			    if (i < linha.length() && linha.charAt(i) == ',') {
-				    if(linha.charAt(i+1) == ','){
-					    separa.add("NaN");
-		}
-           			 i++;  // Pula a vírgula
-
-       			}else{
-
-					StringBuilder aux = new StringBuilder();
-					while(i < linha.length() && linha.charAt(i) != ','){
-						aux.append(linha.charAt(i));
-						i++;
-					}
-				separa.add(aux.toString());
-			
-
-			}
-			
-		//	if(separa.size() == 11){
-		//		break;
-		//	}
-		}
-	}
-
-
-		show.setID(separa.get(0));
-		show.setTYPE(separa.get(1));
-		show.setTITLE(separa.get(2));
-		show.setDIRECTOR(separa.get(3));
-		show.setCAST(splitArrays(separa.get(4)));
-		show.setCOUNTRY(separa.get(5));
-		show.setDATE(separa.get(6));
-		show.setRELEASE_YEAR(Integer.parseInt(separa.get(7)));
-		show.setRATING(separa.get(8));
-		show.setDURATION(separa.get(9));
-		show.setLISTED_IN(splitArrays(separa.get(10)));
-		show.setDESCRIPTION(separa.get(11));
-
-
-		return show;
-	}
-
-	//Função para ler o arquivo CSV e retorna numa String.
-	public static ArrayList <SHOW> leArquivo() {
-		String linha = "", conteudo = "";
-		int aux = 0;
-		ArrayList <SHOW> show = new ArrayList<>();
-
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("disneyplus.csv"));
-			while ((linha = br.readLine()) != null) {
-				
-				if(aux >= 1 && aux <= 5){
-					show.add(atribuiValores(linha));
-				}
-
-				aux++;
-			}
-		} catch (Exception e) {
-			System.out.println("Erro!!");
-		}
-
-		return show;
-	}
-
-	//Func Main
-	public static void main(String args[]) {
-		String conteudo = "";
-		Scanner sc = new Scanner(System.in);
-		ArrayList <SHOW> show = leArquivo();
-		int id;
-		System.out.println("Digite o id para retornar o titulo:");
-		id = sc.nextInt();
-		System.out.println(show.get(id - 1).TITLE);
-
-		for (int i = 0; i < show.get(id-1).CAST.size() + 1;i++){
-			System.out.println("Genero:" + show.get(id-1).LISTED_IN.get(i));
-		}
-
-	}
+        sc.close();
+    }
 }
+
